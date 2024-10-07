9698deaf4ac9b8c2ac0ba485d2fa297fae88a53a
william-pickering-apex <William.Pickering@apexspace.com>
2024-10-07 14:37:19 -0700

    Refactored to have BKP_9141 header file

--- BKP_9141.py
+++ BKP_9141.py
@@ -3,6 +3,8 @@ from unittest.mock import DEFAULT
 import pyvisa
 import time
 
+from numpy.f2py.auxfuncs import throw_error
+
 
 def pull_current(my_instrument_in):
     my_instrument_in.write('MEASure:CURRent?')
@@ -61,19 +63,27 @@ def battery_triangle_wave(my_instrument_in, csv_file_name,min_voltage=20,max_vol
     time_at_voltage=20
     #CSV Format:
     #TIME, V, I_AVG, I_MIN, I_MAX
-    with open(csv_file_name, mode='a') as file:
-        #Step through the voltage range
-        for i in range(5):
-            #set the PSU output Voltage
-            my_instrument_in.query('VOLT {}'.format(min(min_voltage+voltage_step*i,max_voltage)))
-            for j in range(time_at_voltage):
-                print('Voltage Setpoint:{}'.format(pull_voltage(my_instrument_in)))
-                #Timestamp entry
-                local_time = time.localtime()
-                file.write("{}:{}:{},".format(local_time.tm_hour, local_time.tm_min, local_time.tm_sec))
-                sampled_current=ave_pull_current(my_instrument_in, 1)
-                file.write("{},{},{},{}\n".format(pull_voltage(my_instrument_in),sampled_current[0],sampled_current[1],sampled_current[2]))
+    try:
+        with open(csv_file_name, mode='a') as file:
+            #Step through the voltage range
+            for i in range(5):
+                #set the PSU output Voltage
+                my_instrument_in.query('VOLT {}'.format(min(min_voltage+voltage_step*i,max_voltage)))
+                for j in range(time_at_voltage):
+                    #Timestamp entry
+                    local_time = time.localtime()
+                    file.write("{}:{}:{},".format(local_time.tm_hour, local_time.tm_min, local_time.tm_sec))
 
+                    #pull data
+                    sampled_voltage = pull_voltage(my_instrument_in)
+                    sampled_current=ave_pull_current(my_instrument_in, 1)
+
+                    #write data to file on console
+                    file.write("{},{},{},{}\n".format(sampled_voltage,sampled_current[0],sampled_current[1],sampled_current[2]))
+                    print('Voltage Setpoint:{}V, Current Max:{}A'.format(sampled_voltage,sampled_current[2]))
+    except Exception as error:
+        file.close()
+        raise
 
 def set_local_time(my_instrument_in):
     local_time = time.localtime()
@@ -84,44 +94,3 @@ def set_local_time(my_instrument_in):
     my_instrument_in.query(
         'SYST:TIME {0:0>2},{1:0>2},{2:0>2}'.format(local_time.tm_hour, local_time.tm_min, local_time.tm_sec))
 
-
-rm = pyvisa.ResourceManager()
-rm.list_resources()
-
-my_instrument = rm.open_resource('USB0::0x3121::0x0002::579I23132::INSTR')
-
-#Put all channels in parallel mode for high current operation
-my_instrument.query('OUTP:PAIR PARA3')
-
-#Ensure output is off
-my_instrument.query('OUTP 0')
-
-file_base_name = input("Enter Test Name: ")+".csv"
-
-#set the time
-set_local_time(my_instrument)
-
-#set datalogging to USB
-my_instrument.query('INIT:DLOG')
-
-##TEST 1 PAH300###
-#set the current
-my_instrument.query('CURR 10') #TARGET VALUE IS 8A
-
-#ENABLE POWER
-my_instrument.query('OUTP 1')
-try:
-    while (True):
-        battery_triangle_wave(my_instrument,file_base_name)
-        # Pause for 5 seconds
-        #time.sleep(time_step)
-
-        #power off PSU
-        #my_instrument.query('OUTP 0')
-        #time.sleep(time_step)
-except KeyboardInterrupt:
-    print("exiting")
-    #set datalogging to USB
-
-    my_instrument.query('ABORt:DLOG')
-    my_instrument.query('OUTP 0')
